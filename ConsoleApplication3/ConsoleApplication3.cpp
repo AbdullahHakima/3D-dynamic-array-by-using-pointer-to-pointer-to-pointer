5ec342bf0063c0471c9e31ca83601a2af9afa2f4
abdullah hakim <abdullah hakim@ABDULLAHHAKIM>
2021-09-02 20:42:21 +0200

    Add project files.

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x = 3, y = 3, z = 2;
+	int*** arr = new int** [x];
+	for (int i = 0; i < x; i++)
+	{
+		arr[i] = new int* [y];
+		for (int j = 0; j < y; j++)
+		{
+			arr[i][j] = new int[z];
+		}
+	}
+	for (int i = 0; i < x; i++)
+	{
+		for (int j = 0; j < y; j++)
+		{
+			for (int k = 0; k < z; k++)
+			{
+				arr[i][j][k] = i + j + k;
+			}
+		}
+	}
+	for (int i = 0; i < x; i++)
+	{
+		for (int j = 0; j < y; j++)
+		{
+			for (int k = 0; k < z; k++)
+			{
+				cout << arr[i][j][k] << "  ";
+			}
+			cout << endl;
+		}
+		cout << endl;
+	}
+	for (int i = 0; i < x; i++)
+	{
+		for (int j = 0; j < y; j++)
+		{
+			delete[]arr[i][j];
+		}
+		delete[]arr[i];
+	}
+	delete[]arr;
+	arr = NULL;
+}
